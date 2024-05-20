229df830707b5faf0bd43214700d89cef0381f45
Виталий Тамилин <vitaliktamilin@gmail.com>
2024-05-20 14:04:01 +0200

    create base models for apps

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Subscribe(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraint = (
+            models.UniqueConstraint(
+                fields=('user', 'author'),
+                name='unique_follower_following'
+            )
+        )
