c5b4b56a76db3d92533c466aecab15eb6af9cc70
Виталий Тамилин <vitaliktamilin@gmail.com>
2024-05-21 13:09:03 +0200

    add djoser

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -22,6 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 
     'recipes.apps.RecipesConfig',
     'users.apps.UsersConfig',
